71fbb4db87f868f51cfd2df0e7639660e04a8493
SkybaMaryna <marinkaskiba@gmail.com>
2023-03-15 11:46:48 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
+const refs = {
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  list: document.querySelector('#boxes'),
+}
+
+refs.createBtn.addEventListener('click', () => { createBoxes(refs.input.value)})
+refs.destroyBtn.addEventListener('click', destroyBoxes)
+
+function createBoxes(amount) {
+  let a = 30
+  let markup = []
+  for (let i = 0; i < amount; i++) {
+  const divEl = document.createElement('div')
+    divEl.style.width = `${a}px`
+    divEl.style.height = `${a}px`
+    divEl.style.backgroundColor = getRandomHexColor()
+    a += 10
+    markup.push(divEl)
+  }
+  refs.list.append(...markup)
+}
+
+function destroyBoxes() {
+  refs.list.innerHTML = ""
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
