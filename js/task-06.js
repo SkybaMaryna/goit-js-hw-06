b3f22e60a7050fc9323cd781c69b00401c1e6606
SkybaMaryna <marinkaskiba@gmail.com>
2023-03-16 15:54:30 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -6,6 +6,7 @@ refs.input.addEventListener('blur', onInputControl)
 
 function onInputControl(event) {
     if (event.currentTarget.value.length == refs.input.dataset.length) {
+    if (event.currentTarget.value.length === Number(refs.input.dataset.length)) {
         refs.input.classList.add('valid')
         refs.input.classList.remove('invalid')
         return
