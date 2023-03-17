8c9097eec8adea3b6cd29bfdf43291a038ed382b
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-03-17 12:13:31 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,6 @@ const refs = {
 refs.input.addEventListener('blur', onInputControl)
 
 function onInputControl(event) {
-    if (event.currentTarget.value.length == refs.input.dataset.length) {
     if (event.currentTarget.value.length === Number(refs.input.dataset.length)) {
         refs.input.classList.add('valid')
         refs.input.classList.remove('invalid')
